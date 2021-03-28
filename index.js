d5ae71d9904ddb449dec99e5ac61e7c4640c4cfd
Roma <roma811@icloud.com>
2021-03-28 04:39:34 +0300

    Local host change

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const whitelist = [
   "http://localhost:3000",
-  "http://localhost:3001",
+  "http://localhost:5000",
   "https://secret-everglades-99629.herokuapp.com/",
 ];
 const corsOptions = {
